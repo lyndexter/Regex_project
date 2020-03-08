c08596d686c54493de6c580f0ba3fadfb84b869e
tiforn <tarasfqx@gmail.com>
2020-03-08 14:21:46 +0200

    change regular expression to correct form

--- src/main/java/ua/lviv/iot/lab6/Redactor.java
+++ src/main/java/ua/lviv/iot/lab6/Redactor.java
@@ -7,19 +7,19 @@ public class Redactor {
 
     public String removeArticle(final String text) {
 
-        Pattern articlePattern = Pattern.compile("( a )|( an )|( the )");
+        Pattern articlePattern = Pattern.compile("( a(?=\\s))|( an(?=\\s))|( the(?=\\s))|(A )|(An )|(The )");
         Matcher matcher = articlePattern.matcher(text);
-        System.out.println(matcher.replaceAll(" "));
-        return matcher.replaceAll(" ");
+        System.out.println(matcher.replaceAll(""));
+        return matcher.replaceAll("");
 
     }
 
     public String removePrepositions(final String text) {
 
-        Pattern prepositionsPattern = Pattern.compile("( or )|( are )|( in )|( out )|( on )");
+        Pattern prepositionsPattern = Pattern.compile("( or(?=\\s))|( are(?=\\s))|( in(?=\\s))|( out(?=\\s))|( on(?=\\s))|(Or )|(Are )|(In )|(Out )|(On )");
         Matcher matcher = prepositionsPattern.matcher(text);
-        System.out.println(matcher.replaceAll(" "));
-        return matcher.replaceAll(" ");
+        System.out.println(matcher.replaceAll(""));
+        return matcher.replaceAll("");
 
     }
 
